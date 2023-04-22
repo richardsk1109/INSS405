aadf416e811a319741dfb85f1fbb3441982be04c
richardsk1109 <124842316+richardsk1109@users.noreply.github.com>
2023-04-22 15:49:20 -0400

    Add files via upload

---
+++ Lab5q3.py
@@ -0,0 +1,39 @@
+def request():
+
+    number1= int(input('input variable number1='))
+    number2= int(input('input variable number2='))
+    number3= int(input('input variable number3='))
+    print('Addition =', addition(number1, number2, number3))
+    print('Average =', average(number1, number2, number3))
+    print('Max =', max(number1, number2, number3))
+    print('Min ='), min(number1,number2, number3)
+
+def addition(number1, number2, number3):
+    sum- int(number1) + int(number2) + int(number3)
+    return sum
+
+def average(number1, number2, number3):
+    avg= int(number1) + int(number2) + int(number3)
+    return avg
+
+def maximum(number1, number2, number3):
+    print(max(number1, number2, number3))
+    return max(number1, number2, number3)
+
+def minimum(number1,number2,number3):
+    print(min(number1, number2, number3))
+    return min(number1,number2,number3)
+
+def product(number1, number2, number3):
+    multiply = int(number1) * int(number2) *int(number3)
+    return multiply
+
+request()
+
+
+
+
+
+
+
+

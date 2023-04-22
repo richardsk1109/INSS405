51d1f606eda9e52a99e112a7158a39614f4ced2a
Kimberly Richards <richardsk1109@students.bowiestate.edu>
2023-04-22 15:46:39 -0400

    commit lab 5

---
+++ Lab 5/Lab5.py
@@ -0,0 +1,14 @@
+def request():
+    num=input('enter number')
+    print(even(num))
+def even(num):
+    if(int(num) % 2 == 0):
+        return('enter number is even')
+    else:
+        return('enter number is odd')
+    request()
+
+
+
+
+
---
+++ Lab 5/Lab5p2.py
@@ -0,0 +1,10 @@
+import random as rand
+def ran ():
+    print(new())
+def new ():
+    for i in range(8):
+        tet= rand.randint(1, 1000)
+        print(tet)
+
+
+
